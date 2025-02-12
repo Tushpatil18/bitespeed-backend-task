81d2b99ffaaa027c67f41432f3c5608cf5b29467
Tushpatil18 <tusharpatil181111@gmail.com>
2025-02-12 19:10:44 +0530

    Updated index.ts to configure the API endpoint

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// Root route
+app.get("/", (req, res) => {
+  res.send("Welcome to the API!");
+});
+
+// API routes
 app.use("/api", identifyRoutes);
 
 app.listen(PORT, () => {
